a2b83862d6ab3ae2dcfca4e3564c7f1d875cc76b
Brandon Sheppard <brandon_r_sheppard@outlook.com>
2019-10-29 15:50:38 -0400

    Initial Commit -- ASP.NET Core Web Application MVC Template

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Snake_Multiplayer.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
